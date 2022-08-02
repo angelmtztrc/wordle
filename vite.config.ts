da88f821bc580cfa9a4680beba87b0e7324c8606
Angel Martinez <angel.martineztrc@gmail.com>
2022-08-02 16:42:50 -0500

    feat:  add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@atoms': path.resolve(__dirname, './src/components/atoms'),
+      '@molecules': path.resolve(__dirname, './src/components/molecules'),
+      '@organisms': path.resolve(__dirname, './src/components/organisms'),
+      '@styles': path.resolve(__dirname, './src/styles')
+    }
+  }
+});
