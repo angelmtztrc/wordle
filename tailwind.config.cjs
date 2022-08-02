5a6cf3b71938558764f9aa613477905f5b999386
Angel Martinez <angel.martineztrc@gmail.com>
2022-08-02 16:50:43 -0500

    feat:  add font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Roboto', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
